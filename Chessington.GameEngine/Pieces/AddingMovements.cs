50428b3a69c7c996925865a1d8d1466cc3b5c96f
Matthew Richards <Matthew.Richards@softwire.com>
2017-02-21 13:44:20 +0000

    Refactoring for test 8

--- Chessington.GameEngine/Pieces/AddingMovements.cs
+++ Chessington.GameEngine/Pieces/AddingMovements.cs
@@ -22,6 +22,64 @@ namespace Chessington.GameEngine.Pieces
             return availableMoves;
         }
 
+        public static List<Square> AddRooksLateralMoves(Board board, Square pieceLocation)
+        {
+            var availableMoves = new List<Square>();
+
+            for (var i = 0; i < GameSettings.BoardSize; i++)
+            {
+                if ((board.GetPiece(Square.At(pieceLocation.Row, i)) == null) || i == pieceLocation.Row)
+                {
+                    availableMoves.Add(Square.At(pieceLocation.Row, i));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            for (var i = 0; i < GameSettings.BoardSize; i++)
+            {
+                if ((board.GetPiece(Square.At(i, pieceLocation.Col)) == null) || (i == pieceLocation.Col))
+                {
+                    availableMoves.Add(Square.At(i, pieceLocation.Col));
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return availableMoves;
+
+            /*var i = 0;
+            var j = 0;
+            
+            
+            do
+            {
+                if (i != pieceLocation.Row)
+                {
+                    availableMoves.Add(Square.At(pieceLocation.Row, i));
+                    i++;
+                }
+
+            } while (i < GameSettings.BoardSize || (board.GetPiece(Square.At(pieceLocation.Row, i)) == null) || (pieceLocation.Row == i));
+            
+            do
+            {
+                if (j != pieceLocation.Col)
+                {
+                    availableMoves.Add(Square.At(j, pieceLocation.Col));
+                    i++;
+                }
+
+            } while (j < GameSettings.BoardSize || (board.GetPiece(Square.At(j, pieceLocation.Col)) == null) || (pieceLocation.Col == j));
+            
+            availableMoves.RemoveAll(s => s == pieceLocation);
+
+            return availableMoves;*/
+        }
+
         public static List<Square> AddDiagonalMoves(Square pieceLocation)
         {
             var availableMoves = new List<Square>();
--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -13,20 +13,9 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            var rook = board.FindPiece(this);
+            var rookLocation = board.FindPiece(this);
 
-            var friendlyPiece = Square.At(4, 7);
-            var lateralMoves = AddingMovements.AddLateralMoves(rook);
-
-            if (board.GetPiece(Square.At(4, 6)) != null)
-            {
-                lateralMoves.RemoveAll(s => s == friendlyPiece);
-                return lateralMoves;
-            }
-            else
-            {
-                return lateralMoves;
-            }
+            return AddingMovements.AddRooksLateralMoves(board, rookLocation);
         }
     }
 }
