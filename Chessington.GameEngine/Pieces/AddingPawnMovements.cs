059ea0d8c1f1fef26b6cbabe5cdfe3f496acd9bf
BetCla <bethany.clarke@softwire.com>
2020-07-14 17:01:19 +0100

    Refactoring for test 8

---
+++ Chessington.GameEngine/Pieces/AddingPawnMovements.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Chessington.GameEngine.Pieces
+{
+    public class AddingPawnMovements
+    {
+        private Pawn pawn;
+
+        public AddingPawnMovements(Pawn pawn)
+        {
+            this.pawn = pawn;
+        }
+
+        public IEnumerable<Square> AddBlackPawnMovements(Board board, Square pieceLocation)
+        {
+            if (board.GetPiece(Square.At(pieceLocation.Row + 1, pieceLocation.Col)) == null)
+            {
+                var availableMoves = new[] {Square.At(pieceLocation.Row + 1, pieceLocation.Col)};
+
+                if ((pieceLocation.Row == 1) &&
+                    (board.GetPiece(Square.At(pieceLocation.Row + 2, pieceLocation.Col)) == null))
+                {
+                    var firstAvailableMove =
+                        availableMoves.Concat(new[] {Square.At(pieceLocation.Row + 2, pieceLocation.Col)});
+                    return firstAvailableMove;
+                }
+                else
+                {
+                    return availableMoves;
+                }
+            }
+            else
+            {
+                return Enumerable.Empty<Square>();
+            }
+        }
+
+        public IEnumerable<Square> AddWhitePawnMovements(Board board, Square pieceLocation)
+        {
+            if (board.GetPiece(Square.At(pieceLocation.Row - 1, pieceLocation.Col)) == null)
+            {
+                var availableMoves = new[] {Square.At(pieceLocation.Row - 1, pieceLocation.Col)};
+
+                if ((pieceLocation.Row == 7) &&
+                    (board.GetPiece(Square.At(pieceLocation.Row - 2, pieceLocation.Col)) == null))
+                {
+                    var firstAvailableMove =
+                        availableMoves.Concat(new[] {Square.At(pieceLocation.Row - 2, pieceLocation.Col)});
+                    return firstAvailableMove;
+                }
+                else
+                {
+                    return availableMoves;
+                }
+            }
+            else
+            {
+                return Enumerable.Empty<Square>();
+            }
+        }
+    }
+}
--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,9 +5,12 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
+        private readonly AddingPawnMovements addingPawnMovements;
+
         public Pawn(Player player)
             : base(player)
         {
+            addingPawnMovements = new AddingPawnMovements(this);
         }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
@@ -15,64 +18,15 @@ namespace Chessington.GameEngine.Pieces
             if (this.Player == Player.White)
             {
                 var whitePiece = board.FindPiece(this);
-                if (board.GetPiece(Square.At(whitePiece.Row - 1, whitePiece.Col)) == null)
-                {
-                    var availableMoves = new[] {Square.At(whitePiece.Row - 1, whitePiece.Col)};
 
-                    if (whitePiece.Row == 7)
-                    {
-                        //if (board[whitePiece.Row, whitePiece.Col] != null)
-                        if (board.GetPiece(Square.At(whitePiece.Row - 2, whitePiece.Col)) == null)
-                        {
-                            var firstAvailableMove =
-                                availableMoves.Concat(new[] {Square.At(whitePiece.Row - 2, whitePiece.Col)});
-                            return firstAvailableMove;
-                        }
-                        else
-                        {
-                            return availableMoves;
-                        }
-                    }
-                    else
-                    {
-                        return availableMoves;
-                    }
-                }
-                else
-                {
-                    return Enumerable.Empty<Square>();
-                }
+                return addingPawnMovements.AddWhitePawnMovements(board, whitePiece);
             }
 
             if (this.Player == Player.Black)
             {
                 var blackPiece = board.FindPiece(this);
-                if (board.GetPiece(Square.At(blackPiece.Row+1, blackPiece.Col)) == null)
-                {
-                    var availableMoves = new[] {Square.At(blackPiece.Row + 1, blackPiece.Col)};
 
-                    if (blackPiece.Row == 1)
-                    {
-                        if (board.GetPiece(Square.At(blackPiece.Row + 2, blackPiece.Col)) == null)
-                        {
-                            var firstAvailableMove =
-                                availableMoves.Concat(new[] {Square.At(blackPiece.Row + 2, blackPiece.Col)});
-                            return firstAvailableMove;
-                        }
-                        else
-                        {
-                            return availableMoves;
-                        }
-                    }
-                    else
-                    {
-                        return availableMoves;
-                    }
-                }
-                else
-                {
-                    return Enumerable.Empty<Square>();
-                }
+                return addingPawnMovements.AddBlackPawnMovements(board, blackPiece);
             }
             else
             {
