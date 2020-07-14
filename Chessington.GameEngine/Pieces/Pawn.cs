3f87a62c2d74e5f62268bcadb7d236e6aa0fda5c
BetCla <bethany.clarke@softwire.com>
2020-07-14 16:48:35 +0100

    Pawns can't move if there's a piece where it wants to go (test works)

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -15,38 +15,68 @@ namespace Chessington.GameEngine.Pieces
             if (this.Player == Player.White)
             {
                 var whitePiece = board.FindPiece(this);
-                var availableMoves = new[] {Square.At(whitePiece.Row - 1, whitePiece.Col)};
-                
-                if (whitePiece.Row == 7)
+                if (board.GetPiece(Square.At(whitePiece.Row - 1, whitePiece.Col)) == null)
                 {
-                    var firstAvailableMove=availableMoves.Concat(new[] {Square.At(whitePiece.Row - 2, whitePiece.Col)});
-                    return firstAvailableMove;
+                    var availableMoves = new[] {Square.At(whitePiece.Row - 1, whitePiece.Col)};
+
+                    if (whitePiece.Row == 7)
+                    {
+                        //if (board[whitePiece.Row, whitePiece.Col] != null)
+                        if (board.GetPiece(Square.At(whitePiece.Row - 2, whitePiece.Col)) == null)
+                        {
+                            var firstAvailableMove =
+                                availableMoves.Concat(new[] {Square.At(whitePiece.Row - 2, whitePiece.Col)});
+                            return firstAvailableMove;
+                        }
+                        else
+                        {
+                            return availableMoves;
+                        }
+                    }
+                    else
+                    {
+                        return availableMoves;
+                    }
                 }
                 else
                 {
-                    return availableMoves;
+                    return Enumerable.Empty<Square>();
                 }
             }
 
             if (this.Player == Player.Black)
             {
                 var blackPiece = board.FindPiece(this);
-                var availableMoves = new[] {Square.At(blackPiece.Row + 1, blackPiece.Col)};
+                if (board.GetPiece(Square.At(blackPiece.Row+1, blackPiece.Col)) == null)
+                {
+                    var availableMoves = new[] {Square.At(blackPiece.Row + 1, blackPiece.Col)};
 
-                if (blackPiece.Row == 1)
-                {
-                    var firstAvailableMove =
-                        availableMoves.Concat(new[] {Square.At(blackPiece.Row + 2, blackPiece.Col)});
-                    return firstAvailableMove;
+                    if (blackPiece.Row == 1)
+                    {
+                        if (board.GetPiece(Square.At(blackPiece.Row + 2, blackPiece.Col)) == null)
+                        {
+                            var firstAvailableMove =
+                                availableMoves.Concat(new[] {Square.At(blackPiece.Row + 2, blackPiece.Col)});
+                            return firstAvailableMove;
+                        }
+                        else
+                        {
+                            return availableMoves;
+                        }
+                    }
+                    else
+                    {
+                        return availableMoves;
+                    }
                 }
-                else 
+                else
                 {
-                    return availableMoves;
+                    return Enumerable.Empty<Square>();
                 }
             }
             else
             {
-                return null;
+                return Enumerable.Empty<Square>();
             }
         }
     }
