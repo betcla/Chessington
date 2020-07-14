382b6316cb3ebbb6dff045f3114b2cb3f24b6867
BetCla <bethany.clarke@softwire.com>
2020-07-14 14:05:02 +0100

    Making pawns move (test 1)

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -5,13 +5,30 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
-            : base(player) { }
+        public Pawn(Player player)
+            : base(player)
+        {
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            var test1 = new[] {Square.At(2, 0), Square.At(6,0)};
-            return test1;
+            if (this.Player == Player.White)
+            {
+                var whitepiece = board.FindPiece(this);
+                var availablemoves = new[] {Square.At(whitepiece.Row - 1, whitepiece.Col)};
+                return availablemoves;
+            }
+
+            if (this.Player == Player.Black)
+            {
+                var blackpiece = board.FindPiece(this);
+                var availablemoves = new[] {Square.At(blackpiece.Row + 1, blackpiece.Col)};
+                return availablemoves;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
