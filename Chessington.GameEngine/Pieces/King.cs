c6835806dbf470091ca479faf862eecaadfb5e08
BetCla <bethany.clarke@softwire.com>
2020-07-14 16:16:07 +0100

    Adding movements for king (test works)

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,18 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var availableMoves = new List<Square>
+            {
+                Square.At(3, 3),
+                Square.At(3, 4),
+                Square.At(3, 5),
+                Square.At(4, 3),
+                Square.At(4, 5),
+                Square.At(5, 3),
+                Square.At(5, 4),
+                Square.At(5, 5)
+            };
+            return availableMoves;
         }
     }
 }
